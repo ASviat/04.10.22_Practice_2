8bfa5c44e6087d5e14993810d5e1c10152897a28
Sviatoslav <ecrprague.asi@gmail.com>
2022-10-04 17:39:39 +0300

    Cosmetics++

--- Task31/Program.cs
+++ Task31/Program.cs
@@ -1,29 +1,29 @@
-﻿int[] array= new int[12];
+﻿int[] array = new int[12];
 
-int[] MakeArray(int []arr)
+int[] MakeArray(int[] arr)
 {
-    Random rnd=new Random();
+    Random rnd = new Random();
     Console.Write("{ ");
-    for (int i=0;i<12;i++)
+    for (int i = 0; i < 12; i++)
     {
-        arr[i]=rnd.Next(-9,10);
+        arr[i] = rnd.Next(-9, 10);
         Console.Write($"{arr[i]} ");
-    } 
+    }
     Console.Write("}");
     return arr;
-   
+
 }
 
-int[] arrray= MakeArray(array);
+int[] arrray = MakeArray(array);
 Console.WriteLine();
-void Sum(int [] arar)
+void Sum(int[] arar)
 {
-    int sumPozitive=default;
-    int sumNegative=default;
-    for (int i=0; i<12;i++)
+    int sumPozitive = default;
+    int sumNegative = default;
+    for (int i = 0; i < 12; i++)
     {
-        if (arar[i]>0) sumPozitive+=array[i];
-        else sumNegative+=array[i];
+        if (arar[i] > 0) sumPozitive += array[i];
+        else sumNegative += array[i];
     }
     Console.WriteLine($"Сумма положительных = {sumPozitive}");
     Console.WriteLine($"Сумма отрицательных = {sumNegative}");
